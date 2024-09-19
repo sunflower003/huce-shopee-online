00062f9e497b225597edf101122bdccee54c8f54
lehyyy <huylee63897@gmail.com>
2024-09-20 06:54:37 +0700

    first

---
+++ src/main/java/Project/demo/ShopeeOnlineApplication.java
@@ -0,0 +1,13 @@
+package Project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeOnlineApplication.class, args);
+	}
+
+}
